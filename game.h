59b1a544858d7d3f17fff849836213a8d99b0c46
Łukasz Strus <61932823+lukasz-strus@users.noreply.github.com>
2024-08-25 15:02:47 +0200

    Create game class

---
+++ game.h
@@ -0,0 +1,17 @@
+#ifndef GAME_H
+#define GAME_H
+#include <QGraphicsView>
+
+class Game:public QGraphicsView
+{
+    Q_OBJECT
+
+public:
+    Game(QWidget *parent = 0);
+
+private:
+
+};
+
+
+#endif // GAME_H
